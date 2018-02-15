a35081048f0d9cfcbe2aa982b2c2566b768cada9
ank160592 <ank16agarwal@gmail.com>
2018-02-15 23:59:22 +0530

    Guru 99 GIT Test demo

---
+++ Selenium_tutorial/src/RC_day1/Rc_day1.java
@@ -0,0 +1,14 @@
+package RC_day1;
+
+import org.openqa.selenium.remote.server.SeleniumServer;
+
+public class Rc_day1 {
+
+	public static void main(String[] args) {
+		
+		SeleniumServer selenium = new SeleniumServer("C:\\Users\\ankitkumagarwal\\Desktop\\Selenium\\selenium-server-standalone-3.8.1.jar");
+		
+
+	}
+
+}
